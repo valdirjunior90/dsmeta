2289b4fdcdd7035e34adfa130d0924205a9c9321
Valdir Junior <slipkinoot@hotmail.com>
2022-07-15 23:37:34 -0300

    Send Notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/requests';
 import './styles.css'
 
-const  NotificationButton = () => {
+interface Props {
+    saleId:number
+}
+
+const  NotificationButton = ({saleId}:Props) => {
+    
+    const handleClick = (id:number) => {
+        axios.get(`${BASE_URL}/sales/${id}/notification`)
+            .then(response=>{
+                console.log("sucesso")
+            })
+    }
+    
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={()=>handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
